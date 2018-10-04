65a36882c19bcb44a7e840b8376ee09a4ad85132
Scott Hadzik <scotthadzik@weber.edu>
2018-10-03 18:59:14 -0600

    fixed syntax

--- Lab2.0/2.0.2_LED_ON.py
+++ Lab2.0/2.0.2_LED_ON.py
@@ -1,1 +1,11 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+import RPi.GPIO as GPIO
+
+PIN_R = 12
+
+GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical pin location
+GPIO.setup(PIN_R, GPIO.OUT)   # Set the physical pin 12 to mode is output
+
+
+while True:
+    GPIO.output(PIN_R, GPIO.HIGH)  # Set the R pin to High(3.3V) to turn on led
