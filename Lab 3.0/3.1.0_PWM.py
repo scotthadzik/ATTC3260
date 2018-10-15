be9ea3cdfabb2f1a3a4adfb97d8f0a89267e609a
Scott Hadzik <scotthadzik@weber.edu>
2018-10-14 19:38:10 -0600

    Added the PWM

---
+++ Lab 3.0/3.1.0_PWM.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import RPi.GPIO as GPIO
+import time # import the library time
+
+PIN_R = 12
+Signal_Frequency = 1 # The frequency of the digital signal
+Signal_Duty_Cycle = 50 # The duty cycle of the digital signal. This is the on-time
+PWM_Pin = GPIO.PWM(PIN_R, Signal_Frequency) # Set the pin to a pulse width modulation digital signal with a set frequency
+
+GPIO.setmode(GPIO.BOARD) # Numbers GPIOs by physical location
+GPIO.setup(PIN_R, GPIO.OUT) # Set the R pin to mode is output
+print ("Start of the program")
+print ("The duty cycle is ", Signal_Duty_Cycle) # Print out the current Duty Cycle
+print ("The frequency is ", Signal_Frequency) # Print out the current Frequency 
+print ("The LED ")
+
+while True:
+    PWM_Pin.Start(Signal_Duty_Cycle)    # Start the pwm on the designated pin with a set duty cycle
+    
+print ("End of program")
+
+GPIO.cleanup() # this ensures a clean exit
--- Lab2.0/2.1.2_LED_Blink.py
+++ Lab2.0/2.1.2_LED_Blink.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 import RPi.GPIO as GPIO
+import time                    # import the library time
 
-LED_PIN_R = 12
+PIN_R = 12
 
 GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
-GPIO.setup(LED_PIN_R, GPIO.OUT)   # Set the R pin to mode is output
+GPIO.setup(PIN_R, GPIO.OUT)   # Set the R pin to mode is output
 
+time.sleep(3)        # wait for 3 seconds
+GPIO.output(PIN_R, GPIO.HIGH)  # Set the R pin to High(3.3V) to turn on led
+time.sleep(3)        # wait for 3 seconds
+GPIO.output(PIN_R, GPIO.LOW)  # Set the R pin to Low(0V) to turn off led
 
-try:
-    while True:
-        GPIO.output(LED_PIN_R, GPIO.HIGH)  # Set the R pin to High(3.3V) to turn on led
-except KeyboardInterrupt:
-    GPIO.cleanup() #clean up the GPIO when exiting
+GPIO.cleanup()  # this ensures a clean exit
