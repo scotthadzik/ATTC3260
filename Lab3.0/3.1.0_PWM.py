997045c08abca1d94528a65767a34bf2abe50742
Scott Hadzik <scotthadzik@weber.edu>
2018-10-15 12:17:06 -0600

    Update Lab3.0/3.1.0_PWM.py

--- Lab3.0/3.1.0_PWM.py
+++ Lab3.0/3.1.0_PWM.py
@@ -6,17 +6,14 @@ PIN_R = 12
 Signal_Frequency = 1 # The frequency of the digital signal
 Signal_Duty_Cycle = 50 # The duty cycle of the digital signal. This is the on-time
 
-
 GPIO.setmode(GPIO.BOARD) # Numbers GPIOs by physical location
 GPIO.setup(PIN_R, GPIO.OUT) # Set the R pin to mode is output
 PWM_Pin = GPIO.PWM(PIN_R, Signal_Frequency) # Set the pin to a pulse width modulation digital signal with a set frequency
 
-
-
 print ("Start of the program")
 print ("The duty cycle is ", Signal_Duty_Cycle) # Print out the current Duty Cycle
 print ("The frequency is ", Signal_Frequency) # Print out the current Frequency 
-print ("The LED ")
+print ("The LED is ON")
 
 while True:
     PWM_Pin.start(Signal_Duty_Cycle)    # Start the pwm on the designated pin with a set duty cycle
