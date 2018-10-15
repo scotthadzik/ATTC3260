c074a130acb79069d20e83fab9d3229b2411b1b6
Scott Hadzik <scotthadzik@weber.edu>
2018-10-15 13:56:56 -0600

    Update Lab3.0/3.1.0_PWM.py

--- Lab3.0/3.1.0_PWM.py
+++ Lab3.0/3.1.0_PWM.py
@@ -3,7 +3,7 @@ import RPi.GPIO as GPIO
 import time # import the library time
 
 PIN_R = 12
-Signal_Frequency = 100 # The frequency of the digital signal
+Signal_Frequency = 1 # The frequency of the digital signal
 Signal_Duty_Cycle = 50 # The duty cycle of the digital signal. This is the on-time
 
 GPIO.setmode(GPIO.BOARD) # Numbers GPIOs by physical location
