715e4ef1a31ee1eed6c23c7ca36b14be4e1d23b0
Scott Hadzik <scotthadzik@weber.edu>
2018-10-22 13:48:03 -0600

    Added lab 4.0

--- Lab2.0/2.1.2_LED_Blink.py
+++ Lab2.0/2.1.2_LED_Blink.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 import RPi.GPIO as GPIO
 import time                    # import the library time
+import sys, termious, tty, os
 
 PIN_R = 12
 
-GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
-GPIO.setup(PIN_R, GPIO.OUT)   # Set the R pin to mode is output
+def setup():
+    GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
+    GPIO.setup(PIN_R, GPIO.OUT)   # Set the R pin to mode is output
+
+
+def keyPressed():
+    fileDir = sys.stdin.fileno()
+    old_settings - termious.tc
 
 time.sleep(3)        # wait for 3 seconds
 GPIO.output(PIN_R, GPIO.HIGH)  # Set the R pin to High(3.3V) to turn on led
