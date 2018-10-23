be824eb2554dcd00fad8bcf73d1d195b73b16ee5
Scott Hadzik <scotthadzik@weber.edu>
2018-10-22 21:15:26 -0600

    add initial low to setup

--- Lab4.0/Lab 4.1.0.py
+++ Lab4.0/Lab 4.1.0.py
@@ -9,7 +9,7 @@ Signal_Frequency = 100 # The frequency of the digital signal
 Signal_Duty_Cycle = 100 # The duty cycle of the digital signal. This is the on-time
 
 GPIO.setmode(GPIO.BOARD) # Numbers GPIOs by physical location
-GPIO.setup(PIN_R, GPIO.OUT) # Set the R pin to mode is output
+GPIO.setup(PIN_R, GPIO.OUT, initial=GPIO.LOW) # Set the R pin to mode is output
 
 PWM_R_Pin = GPIO.PWM(PIN_R, Signal_Frequency) # Set the pin to a pulse width modulation digital signal with a set frequency
 
