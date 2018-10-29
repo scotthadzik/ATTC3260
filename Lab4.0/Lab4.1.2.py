95d2d60371bd982cf28bd6052896fa5edb5ec060
Scott Hadzik <scotthadzik@weber.edu>
2018-10-29 13:32:51 -0600

    fixed para

--- Lab4.0/Lab4.1.2.py
+++ Lab4.0/Lab4.1.2.py
@@ -41,9 +41,9 @@ PWM_R_Pin = GPIO.PWM(PIN_R, Red_Signal_Frequency)   # Set the pin to a pulse wid
 PWM_G_Pin = GPIO.PWM(PIN_G, Green_Signal_Frequency) # Set the pin to a pulse width modulation digital signal with a set frequency
 PWM_B_Pin = GPIO.PWM(PIN_B, Blue_Signal_Frequency)  # Set the pin to a pulse width modulation digital signal with a set frequency
 
-print ("Set R to " + str(Red_Signal_Duty_Cycle * (colorOptions)  + ' With a duty cyle of ' + str(Red_Signal_Duty_Cycle   * 100)))
-print ("Set G to " + str(Green_Signal_Duty_Cycle * (colorOptions)+ ' With a duty cyle of ' + str(Blue_Signal_Duty_Cycle  * 100)))
-print ("Set B to " + str(Blue_Signal_Duty_Cycle * (colorOptions) + ' With a duty cyle of ' + str(Green_Signal_Duty_Cycle * 100)))
+print ("Set R to " + str(Red_Signal_Duty_Cycle * (colorOptions))  + ' With a duty cyle of ' + str(Red_Signal_Duty_Cycle   * 100))
+print ("Set G to " + str(Green_Signal_Duty_Cycle * (colorOptions))+ ' With a duty cyle of ' + str(Blue_Signal_Duty_Cycle  * 100))
+print ("Set B to " + str(Blue_Signal_Duty_Cycle * (colorOptions)) + ' With a duty cyle of ' + str(Green_Signal_Duty_Cycle * 100))
 
 while True:
     try: # runs until Ctrl+C interupts
