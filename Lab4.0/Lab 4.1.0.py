2094f3a0750001a625b1805469ef69e18274ccad
Scott Hadzik <scotthadzik@weber.edu>
2018-10-29 11:31:37 -0600

    Fixed Red Signal

--- Lab4.0/Lab 4.1.0.py
+++ Lab4.0/Lab 4.1.0.py
@@ -4,18 +4,18 @@ import time
 
 PIN_R = 12
 
-Signal_Frequency = 2000 # The frequency of the digital signal
-Signal_Duty_Cycle = 50 # The duty cycle of the digital signal. This is the on-time
+Red_Signal_Frequency = 2000 # The frequency of the digital signal
+Red_Signal_Duty_Cycle = 50 # The duty cycle of the digital signal. This is the on-time
 
 GPIO.setmode(GPIO.BOARD) # Numbers GPIOs by physical location
 GPIO.setup(PIN_R, GPIO.OUT) # Set the R pin to mode is output
 
 GPIO.output (PIN_R, GPIO.HIGH) # Turn off the LED
 
-PWM_R_Pin = GPIO.PWM(PIN_R, Signal_Frequency) # Set the pin to a pulse width modulation digital signal with a set frequency
+PWM_R_Pin = GPIO.PWM(PIN_R, Red_Signal_Frequency) # Set the pin to a pulse width modulation digital signal with a set frequency
 
 while True:
-    PWM_R_Pin.start(Signal_Duty_Cycle)    # Start the pwm on the designated pin with a set duty cycle
+    PWM_R_Pin.start(Red_Signal_Duty_Cycle)    # Start the pwm on the designated pin with a set duty cycle
 	
 print ("End of program")
 
