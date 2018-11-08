8b23f18083850a049f29a25431266b9452e3320c
Scott Hadzik <scotthadzik@weber.edu>
2018-11-07 21:08:23 -0700

    changed pwm location

--- Lab4.0/Lab4.1.3.1.py
+++ Lab4.0/Lab4.1.3.1.py
@@ -10,6 +10,7 @@ setRedColor = 256
 Red_Signal_Frequency   = 2000 # The frequency of the digital signal
 Red_Signal_Duty_Cycle = 100
 redColorOFFTime = 0
+PWM_R_Pin = GPIO.PWM(PIN_R, Red_Signal_Frequency)   # Set the pin to a pulse width modulation digital signal with a set frequency
 #------------------SET PIN to OUTPUT -----------------------------------------------------
 GPIO.setmode(GPIO.BOARD) # Numbers GPIOs by physical location
 GPIO.setup(PIN_R, GPIO.OUT) # Set the R pin to mode is output
@@ -24,20 +25,12 @@ def setTheDutyCycle():
     redColorOFFTime   = (setRedColor  / colorOptions) * 100 # Calculate the off time by dividing the setColor by 256. 
     Red_Signal_Duty_Cycle   = 100 - redColorOFFTime   # The duty cycle of the digital signal. This is the on-time
 
-def setPWM():
-    PWM_R_Pin = GPIO.PWM(PIN_R, Red_Signal_Frequency)   # Set the pin to a pulse width modulation digital signal with a set frequency
-
 def printInfo():
         print ("Set R to " + str(setRedColor)  + 
             ' With a duty cycle of ' + str(Red_Signal_Duty_Cycle)  + 
             ' OFF time ' + str(redColorOFFTime))
 
-
-
-
-
 while True:
-    setPWM()
     setTheColor()
     setTheDutyCycle()
     printInfo()
