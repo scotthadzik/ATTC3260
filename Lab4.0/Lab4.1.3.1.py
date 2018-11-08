a31b117b5a6b2b7d491bfe36ebd1a8330445e958
Scott Hadzik <scotthadzik@weber.edu>
2018-11-07 21:26:09 -0700

    fixed global

--- Lab4.0/Lab4.1.3.1.py
+++ Lab4.0/Lab4.1.3.1.py
@@ -3,21 +3,23 @@ import RPi.GPIO as GPIO
 import time
 
 
+
 def setup():
-    global PIN_R = 11
-    global colorOptions = 256 # The number of colors available in an 8 bit number
-    global setRedColor = 256
+    global PIN_R, colorOptions, Red_Signal_Duty_Cycle, Red_Signal_Duty_Cycle, redColorOFFTime, PWM_R_Pin, setRedColor
+    PIN_R = 11
+    colorOptions = 256 # The number of colors available in an 8 bit number
+    setRedColor = 256
 
     # ------------------SET THE FREQUENCY and Duty Cycle -----------------------------------------------------
-    global Red_Signal_Frequency   = 2000 # The frequency of the digital signal
-    global Red_Signal_Duty_Cycle = 100
-    global redColorOFFTime = 0
+    Red_Signal_Frequency   = 2000 # The frequency of the digital signal
+    Red_Signal_Duty_Cycle = 100
+    redColorOFFTime = 0
 
     #------------------SET PIN to OUTPUT -----------------------------------------------------
     GPIO.setmode(GPIO.BOARD) # Numbers GPIOs by physical location
     GPIO.setup(PIN_R, GPIO.OUT) # Set the R pin to mode is output
     GPIO.output (PIN_R, GPIO.HIGH) # Turn off the LED
-    global PWM_R_Pin = GPIO.PWM(PIN_R, Red_Signal_Frequency)   # Set the pin to a pulse width modulation digital signal with a set frequency
+    PWM_R_Pin = GPIO.PWM(PIN_R, Red_Signal_Frequency)   # Set the pin to a pulse width modulation digital signal with a set frequency
 
 #Set the color value
 def setTheColor():
