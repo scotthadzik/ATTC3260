9dbf3373f2098c83a8525302ce8dad1ab3217fd4
Scott Hadzik <scotthadzik@weber.edu>
2018-11-09 15:41:57 -0700

    cleaned up the code

--- Lab4.0/Lab4.1.3.1.py
+++ Lab4.0/Lab4.1.3.1.py
@@ -8,24 +8,19 @@ def setup():
     global PIN_R, colorOptions, Red_Signal_Duty_Cycle, Red_Signal_Duty_Cycle, redColorOFFTime, PWM_R_Pin, setRedColor
     PIN_R = 11
     colorOptions = 256 # The number of colors available in an 8 bit number
-    setRedColor = 256
+    setRedColor = 256 # set the default color prior to getting user input
 
     # ------------------SET THE FREQUENCY and Duty Cycle -----------------------------------------------------
     Red_Signal_Frequency   = 2000 # The frequency of the digital signal
-    Red_Signal_Duty_Cycle = 100
-    redColorOFFTime = 0
+    Red_Signal_Duty_Cycle = 100 # set the default duty cycle prior to getting user input
+    redColorOFFTime = 0 # set the default off time prior to getting user input
 
     #------------------SET PIN to OUTPUT -----------------------------------------------------
     GPIO.setmode(GPIO.BOARD) # Numbers GPIOs by physical location
     GPIO.setup(PIN_R, GPIO.OUT) # Set the R pin to mode is output
     GPIO.output (PIN_R, GPIO.HIGH) # Turn off the LED
     PWM_R_Pin = GPIO.PWM(PIN_R, Red_Signal_Frequency)   # Set the pin to a pulse width modulation digital signal with a set frequency
-    PWM_R_Pin.start(Red_Signal_Duty_Cycle)
-
-#Set the color value
-def setTheColor():
-    global setRedColor
-    setRedColor = int(input("What is the color value for red? (0 to 255) "))
+    PWM_R_Pin.start(Red_Signal_Duty_Cycle) # Set the initial duty cycle
 
 def setTheDutyCycle(): 
     global Red_Signal_Duty_Cycle
@@ -40,13 +35,13 @@ def printInfo():
         print ("Set R to " + str(setRedColor)  + 
             ' With a duty cycle of ' + str(Red_Signal_Duty_Cycle)  + 
             ' OFF time ' + str(redColorOFFTime))
-setup()
+setup() # call the setup function
 while True:
     
     try: # runs until Ctrl+C interupts
-        setRedColor = int(input("What is the color value for red? (0 to 255) "))
-        setTheDutyCycle()
-        printInfo()
+        setRedColor = int(input("What is the color value for red? (0 to 255) ")) 
+        setTheDutyCycle() # call the Duty Cycle method
+        printInfo() # call the print function
     except KeyboardInterrupt: # runs when Ctrl+C interupts
         break
 
