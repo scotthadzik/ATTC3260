730f0569c5acff57f128337187ae39d9d0f3390a
Scott Hadzik <scotthadzik@weber.edu>
2018-12-01 19:07:04 -0700

    added callout

--- Lab5.0/Lab5.0.1.py
+++ Lab5.0/Lab5.0.1.py
@@ -8,9 +8,14 @@ def setup():
 	GPIO.setmode(GPIO.BOARD)
 	GPIO.setup(pullUpResistorPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 	GPIO.setup(pullDownResistorPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+	GPIO.add_event_detect(pullUpResistorPin, GPIO.BOTH, callback=pullUpSense, bouncetime=200)
+	GPIO.add_event_detect(pullDownResistorPin, GPIO.BOTH, callback=pullDownSense, bouncetime=200)
 
-def detect(chn):
-	print('*   Button Pressed!   *')
+def pullUpSense(chn):
+	print('*   Pull UP Sense   *')
+
+def pullDownSense(chn):
+	print('*   Pull DOWN Sense   *')
 
 setup()
 while True:
