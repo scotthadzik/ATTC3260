5e3257f67a09f4ff0a774bcbaa8d82f83e7eaf89
Scott Hadzik <scotthadzik@weber.edu>
2018-12-01 19:51:27 -0700

    changed order

--- Lab5.0/Lab5.0.0.py
+++ Lab5.0/Lab5.0.0.py
@@ -1,28 +1,17 @@
 #!/usr/bin/env python
 import RPi.GPIO as GPIO
 
-pullUpResistorPin = 11
-pullDownResistorPin = 13
+inputPin = 11
 
 def setup():
 	GPIO.setmode(GPIO.BOARD)
-	GPIO.setup(pullUpResistorPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-	GPIO.setup(pullDownResistorPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-	GPIO.add_event_detect(pullUpResistorPin, GPIO.BOTH, callback=pullUpSense, bouncetime=200)
-	GPIO.add_event_detect(pullDownResistorPin, GPIO.BOTH, callback=pullDownSense, bouncetime=200)
-
-def pullUpSense(chn):
-	print('*   Pull UP Sense   *')
-
-def pullDownSense(chn):
-	print('*   Pull DOWN Sense   *')
+	GPIO.setup(inputPin, GPIO.IN)
 
 setup()
 while True:
-	try:
-		pass
-	except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
-		break
+	print (GPIO.input(inputPin))
+except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
+	break
 
 GPIO.cleanup()                     # Release resource
 
