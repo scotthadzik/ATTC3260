cb98c665a6a7c24d229386a08ca1bc369883230d
Scott Hadzik <scotthadzik@weber.edu>
2019-11-02 22:51:29 -0600

    LED on and off

--- Lab6/Lab6.1.4.py
+++ Lab6/Lab6.1.4.py
@@ -1,28 +1,30 @@
 #!/usr/bin/env python
 import RPi.GPIO as GPIO
 
-pullUPResistorPin = 11
+button = 11
 PIN_R = 12
 
 
 def setup():
 	GPIO.setmode(GPIO.BOARD)
 
-	GPIO.setup(pullUPResistorPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-	GPIO.add_event_detect(pullUPResistorPin, GPIO.BOTH, callback=buttonSense, bouncetime=500)
+	GPIO.setup(button, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	GPIO.add_event_detect(button, GPIO.BOTH, callback=buttonSense, bouncetime=500)
 
 	GPIO.setup(PIN_R, GPIO.OUT)
 	GPIO.output(PIN_R, GPIO.LOW)
+	
 
-def LED (state):
-    if state == 0:                  # if the state is low
-        GPIO.output(PIN_R, GPIO.LOW)         # turn off LED
-    if state == 1:                  #if the sate is high
-        GPIO.output(PIN_R, GPIO.HIGH)        #Turn on LED
+def LED(state):
+	if state == 0:	#	The button has been pressed
+		GPIO.output(PIN_R, GPIO.HIGH) # Turn on LED
+	if state == 1: 	#	The button has been released
+		GPIO.output(PIN_R, GPIO.LOW) # Turn off LED
 
-def buttonSense(chn):
-	LED(GPIO.input(pullUPResistorPin))
-	GPIO.output(PIN_R, GPIO.HIGH)  # Set the R pin to High(3.3V) to turn on led
+
+def buttonSense(channel):
+	print('*   Button Pressed   *')
+	LED(GPIO.input(button))
 
 def loop():
 	while True:
