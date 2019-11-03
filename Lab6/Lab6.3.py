11eefe698c2a31abe41310401360d920a6c73fc0
Scott Hadzik <scotthadzik@weber.edu>
2019-11-02 19:58:40 -0600

    added led support

---
+++ Lab6/Lab6.3.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import RPi.GPIO as GPIO
+
+pullUPResistorPin = 11
+PIN_R = 12
+
+
+def setup():
+	GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(PIN_R, GPIO.OUT)   # Set the physical pin 12 to mode is output
+	GPIO.setup(pullUPResistorPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	GPIO.add_event_detect(pullUPResistorPin, GPIO.BOTH, callback=buttonSense, bouncetime=500)
+
+def buttonSense(chn):
+	print('*   Button Pressed   *')
+    GPIO.output(PIN_R, GPIO.HIGH)  # Set the R pin to High(3.3V) to turn on led
+
+setup()
+while True:
+	try:
+		pass
+	except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
+		break
+
+GPIO.cleanup()                     # Release resource
