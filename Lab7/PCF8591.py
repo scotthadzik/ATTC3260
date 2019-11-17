50e4f2de8b4ee2ba7e7f415eccc9cd1c2882e4b9
Scott Hadzik <scotthadzik@weber.edu>
2019-11-16 21:23:24 -0700

    updated print

--- Lab7/PCF8591.py
+++ Lab7/PCF8591.py
@@ -39,8 +39,8 @@ def read(chn): #channel
 			bus.write_byte(address,0x43)
 		bus.read_byte(address) # dummy read to start conversion
 	except Exception as e:
-		print "Address: %s" % address
-		print e
+		print ("Address: " + str(address))
+		print (e)
 	return bus.read_byte(address)
 
 def write(val):
@@ -50,14 +50,14 @@ def write(val):
 		# print temp to see on terminal else comment out
 		bus.write_byte_data(address, 0x40, temp)
 	except Exception, e:
-		print "Error: Device address: 0x%2X" % address
-		print e
+		print ("Error: Device address: 0x" +  address)
+		print (e)
 
 if __name__ == "__main__":
 	setup(0x48)
 	while True:
-		print 'AIN0 = ', read(0)
-		print 'AIN1 = ', read(1)
+		print ('AIN0 = ' + read(0))
+		print ('AIN1 = ' + read(1))
 		tmp = read(0)
 		tmp = tmp*(255-125)/255+125 # LED won't light up below 125, so convert '0-255' to '125-255'
 		write(tmp)
